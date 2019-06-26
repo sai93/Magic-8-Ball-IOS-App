511df547c560e706795138ce296685320982e0b9
iamspeed <iamspeed32@gmail.com>
2019-06-25 22:12:21 -0700

    Initial Magic 8 Ball Template

---
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by iamspeed on 6/25/19.
+//  Copyright © 2019 Sai Manideep J. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
